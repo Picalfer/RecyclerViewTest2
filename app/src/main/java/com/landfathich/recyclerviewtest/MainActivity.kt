6515070f5272b922ec43213afd73aeb84020315b
Arthur Kozhemyakin <trable228@icloud.com>
2023-06-12 11:58:01 +0300

    simple linear rv 1 to 100 numbers

--- app/src/main/java/com/landfathich/recyclerviewtest/MainActivity.kt
+++ app/src/main/java/com/landfathich/recyclerviewtest/MainActivity.kt
@@ -2,10 +2,17 @@ package com.landfathich.recyclerviewtest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+
+        val adapter = MyAdapter()
+        recyclerView.adapter = adapter
+
     }
 }
---
+++ app/src/main/java/com/landfathich/recyclerviewtest/MyAdapter.kt
@@ -0,0 +1,26 @@
+package com.landfathich.recyclerviewtest
+
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+
+class MyAdapter: RecyclerView.Adapter<MyAdapter.MyViewHolder>() {
+
+    class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        return MyViewHolder(TextView(parent.context))
+    }
+
+    override fun getItemCount(): Int = 100
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        (holder.itemView as TextView).text = "${position + 1}"
+    }
+
+}
+
